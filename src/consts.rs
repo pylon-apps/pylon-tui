c7c0769a4a48de0750bec6df196638c0db52f336
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-09-12 22:05:42 +0530

    feat: Initialize basic console app

---
+++ src/consts.rs
@@ -0,0 +1,7 @@
+pub(crate) const LOGO: &str = r"
+PPPPPP  YY   YY LL       OOOOO  NN   NN
+PP   PP YY   YY LL      OO   OO NNN  NN
+PPPPPP   YYYYY  LL      OO   OO NN N NN
+PP        YYY   LL      OO   OO NN  NNN
+PP        YYY   LLLLLLL  OOOO0  NN   NN
+";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod consts;
+
+use cursive::views::{Dialog, TextView};
+use cursive::align::HAlign;
+use cursive::view::Margins;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = cursive::default();
+    let logo = TextView::new(consts::LOGO);
+
+    app.add_layer(Dialog::around(logo)
+        .title("Pylon")
+        .h_align(HAlign::Center)
+        .padding(Margins::lrtb(5, 5, 2, 2))
+        .button("Send", |s| s.quit())
+        .button("Receive", |s| s.quit()));
+
+    app.run();
 }
