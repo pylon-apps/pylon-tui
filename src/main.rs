ee0c37e5363a55e94693d1f10359d63c07c4ce65
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-09-14 23:21:51 +0530

    feat: Define basic app layout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod consts;
 
-use cursive::views::{Dialog, TextView};
+use cursive::views::{Dialog, LinearLayout, SelectView, TextView};
 use cursive::align::HAlign;
 use cursive::view::Margins;
 
@@ -8,12 +8,23 @@ fn main() {
     let mut app = cursive::default();
     let logo = TextView::new(consts::LOGO);
 
-    app.add_layer(Dialog::around(logo)
-        .title("Pylon")
+    let mut menu = SelectView::<&str>::new();
+    let dashboard = Dialog::around(logo)
         .h_align(HAlign::Center)
         .padding(Margins::lrtb(5, 5, 2, 2))
         .button("Send", |s| s.quit())
-        .button("Receive", |s| s.quit()));
+        .button("Receive", |s| s.quit());
+
+    menu.add_item("Dashboard", "dashboard");
+    menu.add_item("Active Transfers", "active_transfers");
+    menu.add_item("History", "history");
+    menu.add_item("Settings", "settings");
+    menu.add_item("About", "settings");
+
+    app.add_layer(Dialog::around(LinearLayout::horizontal()
+        .child(Dialog::around(menu))
+        .child(dashboard))
+        .title("Pylon"));
 
     app.run();
 }
