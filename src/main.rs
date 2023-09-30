d6c1c31b0f6031b20d348fd08bf54dfd454aa318
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-09-30 22:01:27 +0530

    feat: Make window full-screen

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 mod consts;
 
+use cursive::traits::*;
 use cursive::views::{Dialog, LinearLayout, SelectView, TextView};
 use cursive::align::HAlign;
 use cursive::view::Margins;
 
 fn main() {
     let mut app = cursive::default();
-    let logo = TextView::new(consts::LOGO);
+    let logo = TextView::new(consts::LOGO).h_align(HAlign::Center);
 
     let mut menu = SelectView::<&str>::new();
     let dashboard = Dialog::around(logo)
         .h_align(HAlign::Center)
         .padding(Margins::lrtb(5, 5, 2, 2))
         .button("Send", |s| s.quit())
-        .button("Receive", |s| s.quit());
+        .button("Receive", |s| s.quit())
+        .full_width();
 
     menu.add_item("Dashboard", "dashboard");
     menu.add_item("Active Transfers", "active_transfers");
@@ -24,7 +26,8 @@ fn main() {
     app.add_layer(Dialog::around(LinearLayout::horizontal()
         .child(Dialog::around(menu))
         .child(dashboard))
-        .title("Pylon"));
+        .title("Pylon")
+        .full_screen());
 
     app.run();
 }
